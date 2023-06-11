a01a569b13df8682dec201120d504130ac024c02
Harsh Singh <70627271+hs5415443@users.noreply.github.com>
2023-06-11 21:05:25 +0530

    Create Recursion_Q2.java

---
+++ Recursion_Q2.java
@@ -0,0 +1,17 @@
+// Write recursive function that takes an array of words and returns an array containing all words capitalized i/p : ["foo", "bar", "world"] o/p : ["FOO", "BAR", "WORLD"] 
+import java.util.Arrays;
+public class WordsCapatilization {
+  static String [] wordsCapatilised(String inputArr [] , int index){ if(index < 0){         
+    return new String[inputArr.length];
+  }
+String [] subResult = wordsCapatilised(inputArr, index -1); 
+subResult[index] = inputArr[index].toUpperCase();         
+return subResult;   
+} 
+  static String [] wordsCapatilised(String inputArr []){   
+    return wordsCapatilised(inputArr , inputArr.length -1); }
+  public static void main(String[] args) 
+  { 
+    String [] inputArr = {"alice", "in" ,"wonderland"}; 
+    System.out.println(Arrays.toString(wordsCapatilised(inputArr))); } 
+} 
