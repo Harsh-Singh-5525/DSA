7e32f14a939b86eccfae5519517f1d774588d830
Harsh Singh <70627271+hs5415443@users.noreply.github.com>
2023-06-11 21:10:22 +0530

    Create Recursion_Q3.java

---
+++ Recursion_Q3.java
@@ -0,0 +1,20 @@
+//Find the Highest Element in an Array  
+public class HighestElement 
+{
+  static int highestElement(int arr[] , int index){ 
+    if(index == 0){   
+      return arr[index]; 
+    } 
+    int highestElem = highestElement(arr, index -1); 
+    if(highestElem < arr[index]){ 
+      highestElem = arr[index];
+    }
+    return highestElem;
+  }
+  static int highestElement(int arr[]){  
+    return highestElement(arr, arr.length -1);
+  } 
+  public static void main(String[] args) {
+    int [] inputArr = {4, 5 , 10 , 2 , 1 ,7}; 
+    System.out.println("Highest element in thearray is: "+highestElement(inputArr));    }
+} 
